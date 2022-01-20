a97ddaf6f0e9c3b65b74d13fa68aee2aa159bf65
Femi Laizer <90426320+femiscript@users.noreply.github.com>
2022-01-20 03:41:39 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//setInterval function to call the function every 1000 milisecond
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+//This is the function code from the above setInterval
+function setClock() {
+    const currentDate = new Date();
+
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
